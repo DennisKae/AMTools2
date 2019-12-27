bec6fe1f2a4b63db9ba79f71f5ac4f1e2fbddeb4
DennisKae <DennisKae@users.noreply.github.com>
2019-12-27 19:59:49 +0100

    SettingsController added

---
+++ src/Web.Core/Services/Settings/Interfaces/IGuiVisibilityService.cs
@@ -0,0 +1,9 @@
+﻿using AMTools.Web.Core.ViewModels;
+
+namespace AMTools.Web.Core.Services.Settings.Interfaces
+{
+    public interface IGuiVisibilityService
+    {
+        GuiVisibilityViewModel GetGuiVisibility();
+    }
+}
